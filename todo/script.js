f531023e6b3258d5b2ce60c5c3f92751af102c18
Yaroslav Maltsev <yaroslavma@tahometer.com>
2022-02-14 12:24:42 +0200

    Added task deleting

--- todo/script.js
+++ todo/script.js
@@ -52,14 +52,13 @@ const list = [
 const taskListElement = document.getElementById("taskList");
 
 function generateTaskDomObject(task) {
-  const { name, text, done, dueDate } = task;
+  const { id, name, text, done, dueDate } = task;
   taskListElement.innerHTML += `
-    <li class="form-check container todolist__task">
+    <li class="form-check container todolist__task" id="${id}" done="${done}" onclick="handleClick(this)">
         <input
           type="checkbox"
           class="form-check-input todolist__task-input" ${done ? "checked" : ""}
-          onclick="changeTaskStatus(this)"
-          done="${done}"
+          
         />
         
         <label class="form-check-label todolist__task"> 
@@ -79,13 +78,25 @@ function generateTaskDomObject(task) {
           dueDate.getDate()
         }
         </span>
+        <button class="todolist__task-delete btn btn-secondary">Delete</button>
     </li>
   `;
 }
 
-function changeTaskStatus(taskElem) {
-  const oldValue = taskElem.getAttribute("done") === "true";
-  taskElem.setAttribute("done", !oldValue);
+function handleClick(taskElem) {
+  const allClasses = event.target.classList;
+  allClasses.includes = [].includes;
+  if (allClasses.includes("form-check-input")) {
+    const oldValue = taskElem.getAttribute("done") === "true";
+    taskElem.setAttribute("done", !oldValue);
+  } else if (allClasses.includes("todolist__task-delete")) {
+    const taskId = parseInt(taskElem.getAttribute("id"));
+    const taskIndex = list.findIndex((task) => task.id === taskId);
+    list.splice(taskIndex, 1);
+
+    taskElem.remove();
+    console.log(list);
+  }
 }
 
 function isDateExpired(date) {
@@ -96,3 +107,15 @@ function isDateExpired(date) {
 }
 
 list.forEach((task) => generateTaskDomObject(task));
+
+const showAllButton = document.getElementById("show-all");
+showAllButton.addEventListener("click", (event) => {
+  const allTasks = taskListElement.children;
+  allTasks.forEach = [].forEach;
+  allTasks.forEach((task) => {
+    const isDone = task.getAttribute("done") === "true";
+    if (isDone) {
+      task.classList.toggle("none");
+    }
+  });
+});
