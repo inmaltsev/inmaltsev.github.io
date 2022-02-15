873b539b39869a2f5f7e882921156939b931eaf2
Yaroslav Maltsev <yaroslavma@tahometer.com>
2022-02-15 15:52:00 +0200

    JsTodo: Connected to backend

--- todo/script.js
+++ todo/script.js
@@ -1,63 +1,47 @@
-const taskList = [
-  {
-    id: 1,
-    listId: 2,
-    name: "Name for task",
-    description: "uncompleted task for today",
-    done: false,
-    dueDate: new Date(),
-  },
-  {
-    id: 2,
-    listId: 2,
-    name: "Another big task",
-    description: "completed task for today",
-    done: true,
-    dueDate: new Date(),
-  },
-  {
-    id: 3,
-    listId: 2,
-    name: "Name for task",
-    description: "completed task for past",
-    done: true,
-    dueDate: new Date("2022-02-12T00:00:00.000Z"),
-  },
-  {
-    id: 4,
-    listId: 2,
-    name: "Name for task",
-    description: "uncompleted task for past",
-    done: false,
-    dueDate: new Date("2022-02-12T00:00:00.000Z"),
-  },
-  {
-    id: 5,
-    listId: 2,
-    name: "Name for task",
-    description: "completed task for past",
-    done: true,
-    dueDate: new Date("2022-02-10T00:00:00.000Z"),
-  },
-  {
-    id: 6,
-    listId: 2,
-    name: "Name for task",
-    description: "uncompleted task for past",
-    done: false,
-    dueDate: new Date("2022-02-10T00:00:00.000Z"),
-  },
-];
+const tasksEndpoint = "http://localhost:3000/lists/5195/tasks";
+
+function getTasksFromServer() {
+  return fetch(tasksEndpoint + "?all=true").then((res) => res.json());
+}
+
+function deleteTaskFromServer(taskId) {
+  fetch(tasksEndpoint + `/${taskId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+function updateTask(editedTask) {
+  fetch(tasksEndpoint + `/${editedTask.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(editedTask),
+  });
+}
+
+function createTask(newTask) {
+  return fetch(tasksEndpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newTask),
+  }).then((res) => {
+    return res.json();
+  });
+}
 
 const taskListElement = document.getElementById("taskList");
 
-// DOM
+// DOM:only
 function addTaskToDom(task) {
-  const { id, name, description, done, dueDate } = task;
-  const isExpired = isDateExpired(dueDate);
-  const dateText = dueDate
-    ? dueDate.getDate() + "-" + dueDate.getMonth() + "-" + dueDate.getFullYear()
-    : "";
+  const { id, text, done, dueDate } = task;
+  const taskDate = new Date(dueDate);
+  const isExpired = isDateExpired(taskDate);
 
   const browserSupportsTemplate =
     "content" in document.createElement("template");
@@ -78,19 +62,34 @@ function addTaskToDom(task) {
 
     checkbox.checked = done;
 
-    nameField.textContent = name;
-    descriptionField.textContent = description;
+    nameField.textContent = "name";
+    descriptionField.textContent = text;
 
     dateField.setAttribute("expired", isExpired);
-    dateField.textContent = dateText;
+    dateField.textContent =
+      taskDate.getFullYear() +
+      "-" +
+      taskDate.getMonth() +
+      "-" +
+      taskDate.getDate();
 
     taskListElement.appendChild(taskElement);
   }
 }
 
+function isDateExpired(date) {
+  if (date) {
+    const now = new Date();
+    now.setHours(0, 0, 0, 0);
+    date.setHours(0, 0, 0, 0);
+    return date < now;
+  } else {
+    return false;
+  }
+}
+
 // EVENT
 function handleClick(taskElem) {
-  console.log("click");
   const allClasses = event.target.classList;
   allClasses.includes = [].includes;
 
@@ -105,30 +104,36 @@ function handleClick(taskElem) {
 // COMB
 function deleteTask(taskElem) {
   const taskId = parseInt(taskElem.getAttribute("id"));
-  const taskIndex = taskList.findIndex((task) => task.id === taskId);
-  taskList.splice(taskIndex, 1);
+  deleteTaskFromServer(taskId);
 
   taskElem.remove();
-  console.log(taskList);
 }
 
 // COMB : update task
 function changeTaskState(taskElem) {
-  const oldValue = taskElem.getAttribute("done") === "true";
-  taskElem.setAttribute("done", !oldValue);
+  const name = taskElem.querySelector(".todolist__task-name").textContent;
+  const text = taskElem.querySelector(
+    ".todolist__task-description"
+  ).textContent;
+  const date = taskElem.querySelector(".todolist__task-date").textContent;
+  const done = taskElem.getAttribute("done") === "true";
+  const id = parseInt(taskElem.getAttribute("id"));
+
+  const newDoneValue = !done;
+
+  const editedTask = {
+    id,
+    name,
+    text,
+    done: newDoneValue,
+    dueDate: new Date(date),
+  };
+  updateTask(editedTask);
+  console.log(editedTask);
+
+  taskElem.setAttribute("done", newDoneValue);
 
   displayTasksAccordingToQuery();
-}
-
-function isDateExpired(date) {
-  if (date) {
-    const now = new Date();
-    now.setHours(0, 0, 0, 0);
-    date.setHours(0, 0, 0, 0);
-    return date < now;
-  } else {
-    return false;
-  }
 }
 
 // DOM
@@ -163,8 +168,8 @@ function makeTaskVisible(task) {
   task.classList.toggle("none", false);
 }
 
-// DOM
-taskList.forEach((task) => addTaskToDom(task));
+// SERVER
+getTasksFromServer().then((res) => res.forEach((task) => addTaskToDom(task)));
 
 // EVENT
 const showOnlyCompletedButton = document.getElementById("show-all");
@@ -172,9 +177,9 @@ showOnlyCompletedButton.addEventListener("click", (event) => {
   displayTasksAccordingToQuery();
 });
 
-// EVENT
+// COMB
 const createTaskForm = document.forms.createTask;
-createTaskForm.addEventListener("submit", (event) => {
+createTaskForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const formData = Object.fromEntries(new FormData(createTaskForm));
@@ -188,8 +193,9 @@ createTaskForm.addEventListener("submit", (event) => {
     const createdTask = Object.assign(formData);
     createdTask.dueDate = formData.dueDate ? new Date(formData.dueDate) : null;
 
-    taskList.push(createdTask);
-    addTaskToDom(createdTask);
+    // SERVER
+    const newTask = await createTask(createdTask);
+    addTaskToDom(newTask);
   } else {
     nameInput.classList.toggle("invalid", true);
   }
